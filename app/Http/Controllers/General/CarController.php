46094b51bc8c97cb2948743f248b6da258a6f651
lucas <visquest@163.com>
2018-12-29 15:54:00 +0800

    car

--- app/Http/Controllers/General/CarController.php
+++ app/Http/Controllers/General/CarController.php
@@ -7,8 +7,11 @@ class CarController extends Controller
 {
     public function index()
     {
-    	
-    	
+        $user_info = \Session::get('user_info');
+        $role_id = is_numeric($user_info['role_id']) ? intval($user_info['role_id']) : explode(',', $user_info['role_id']);
+
+        if ()
+
         return view('general/car', array());
     }
 }
---
+++ app/Models/CarModel.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Models;
+
+class CarModel extends BaseModel
+{
+    protected $_table = 'sys_menu';
+	
+}
